daa7cc47918497b0a66d83acded08707ffe0ae86
Jonas <jriedel@united-internet.de>
2021-12-03 11:41:26 +0100

    Merged both endpoints in once

--- index.js
+++ index.js
@@ -4,49 +4,62 @@ const PORT = 55690
 
 const app = express()
 
-let getIncidence = async function(xpath) {
-    try {
-        const URL = 'https://corona.karlsruhe.de/'
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
-        let spanElement;
+let getIncidence = async function () {
+  try {
+    const URL = 'https://corona.karlsruhe.de/'
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    let stadtkreis
+    let landkreis
 
-        await page.goto(URL)
+    await page.goto(URL)
 
-        spanElement = await page.$x(xpath)
-        spanElement = spanElement.pop()
-        spanElement = await spanElement.getProperty('innerText')
-        
-        return spanElement = await spanElement.jsonValue().then(spanElement => {
-            return spanElement
-        })
-    } catch (error) {
-        console.log(error)
+    stadtkreis = await page.$x(
+      '/html/body/article/div[3]/div/div[2]/p[2]/strong'
+    )
+    stadtkreis = stadtkreis.pop()
+    stadtkreis = await stadtkreis.getProperty('innerText')
+
+    landkreis = await page.$x(
+      '/html/body/article/div[3]/div/div[3]/p[2]/strong'
+    )
+    landkreis = landkreis.pop()
+    landkreis = await landkreis.getProperty('innerText')
+
+    let result = {
+      stadtkreis: stadtkreis,
+      landkreis: landkreis,
     }
+
+    return result
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 app.get('/', (_, res) => {
+  res.send({
+    msg: 'Hello World',
+  })
+})
+
+let incidence = getIncidence().then((result) => {
+  const landkreisIncidence = Math.round(
+    parseFloat(result.landkreis._remoteObject.value)
+  )
+
+  const stadtkreisIncidence = Math.round(
+    parseFloat(result.stadtkreis._remoteObject.value)
+  )
+
+  app.get('/incidence', (req, res) => {
     res.send({
-        msg: 'Hello World'
+      stadtkreis: stadtkreisIncidence,
+      landkreis: landkreisIncidence,
     })
-})
-
-let landkreis = getIncidence('/html/body/article/div[3]/div/div[3]/p[2]/strong').then(function(result) {
-    app.get('/incidence/landkreis', (_, res) => {
-        res.send({
-            incidence: result
-        })
-    })
-})
-
-let stadtkreis = getIncidence('/html/body/article/div[3]/div/div[2]/p[2]/strong').then(function(result) {
-    app.get('/incidence/stadtkreis', (_, res) => {
-        res.send({
-            incidence: result
-        })
-    })
+  })
 })
 
 app.listen(PORT, () => {
-    console.log(`-> Server started on port ${PORT}`)
+  console.log(`-> Server started on port ${PORT}`)
 })
